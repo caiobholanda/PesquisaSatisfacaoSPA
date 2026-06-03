4ab2fc8dd58b8f33b5ad45a6042e2f4ba0e84765
Caio Barsi <estagio.ti@granmarquise.com.br>
2026-06-03 13:07:09 -0300

    auto: 2026-06-03 13:07:09

--- public/js/admin.js
+++ public/js/admin.js
@@ -1182,7 +1182,9 @@ function calVerDetalhes(id) {
     <div class="resdet-section">
       <div class="resdet-section-title">Tratamento</div>
       <div class="resdet-row"><span class="resdet-label">Serviço</span>${r.tratamento ? `<span class="resdet-value gold">${r.tratamento}</span>` : '<span class="resdet-value empty">não informado</span>'}</div>
-      <div class="resdet-row"><span class="resdet-label">Duração</span><span class="resdet-value mono">${dur} min</span></div>
+      ${r.linha ? `<div class="resdet-row"><span class="resdet-label">Linha</span><span class="resdet-value">${r.linha}</span></div>` : ''}
+      <div class="resdet-row"><span class="resdet-label">Bloco</span><span class="resdet-value mono">${dur} min · ${dur/30} slot${dur/30>1?'s':''}</span></div>
+      ${_renderResDetComboPreco(r)}
     </div>
 
     <div class="resdet-section">
